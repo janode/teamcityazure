0eea09c7908f0677bb74759ed4f58adbca8ffa5d
janode <janode@gmail.com>
2012-12-14 23:31:29 +0100

    clean up

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient();
+            client.Headers.Add("User-Agent", "Nobody");
+            var response = client.DownloadString(new Uri("http://www.smallestdotnet.com/smallestdotnet/javascriptdom.ashx"));
+            dynamic t = JsonConvert.DeserializeObject<SmallestDotNetThing>(response);
+
+            var settings = new JsonSerializerSettings();
+            settings.Converters.Add(new IsoDateTimeConverter());
+            
+        }
+
+        public class SmallestDotNetThing
+        {
+            public DotNetVersion latestVersion { get; set; }
+            public List<DotNetVersion> allVersions { get; set; }
+            public List<DotNetVersion> downloadableVersions { get; set; }
+        }
+
+        public class DotNetVersion
+        {
+            public int major { get; set; }
+            public int minor { get; set; }
+            public string profile { get; set; }
+            public int? servicePack { get; set; }
+            public string url { get; set; }
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int iterations = 10000;
+            while (true)
+            {
+                Run("Uncontended AsyncLock ", () => TestAsyncLockAsync(iterations, false));
+                Run("Uncontended AwCritSec ", () => TestAwaitableCriticalSectionAsync(iterations, false));
+                Run("Contended   AsyncLock ", () => TestAsyncLockAsync(iterations, true));
+                Run("Contended   AwCritSec ", () => TestAwaitableCriticalSectionAsync(iterations, true));
+                Console.WriteLine();
+            }
+        }
+
+        static void Run(string name, Func<Task> test)
+        {
+            var sw = Stopwatch.StartNew();
+            test().Wait();
+            sw.Stop();
+            Console.WriteLine("{0}: {1}ms", name, sw.ElapsedMilliseconds);
+        }
+
+        static async Task TestAsyncLockAsync(int iterations, bool isContended)
+        {
+            var mutex = new AsyncLock();
+            if (isContended)
+            {
+                var waits = new Task<IDisposable>[iterations];
+                using (await mutex.LockAsync())
+                    for (int i = 0; i < iterations; i++)
+                        waits[i] = mutex.LockAsync();
+                for (int i = 0; i < iterations; i++)
+                    using (await waits[i]) { }
+            }
+            else
+            {
+                for (int i = 0; i < iterations; i++)
+                    using (await mutex.LockAsync()) { }
+            }
+        }
+
+        static async Task TestAwaitableCriticalSectionAsync(int iterations, bool isContended)
+        {
+            var mutex = new AwaitableCriticalSection();
+            if (isContended)
+            {
+                var waits = new Task<IDisposable>[iterations];
+                using (await mutex.EnterAsync())
+                    for (int i = 0; i < iterations; i++)
+                        waits[i] = mutex.EnterAsync();
+                for (int i = 0; i < iterations; i++)
+                    using (await waits[i]) { }
+            }
+            else
+            {
+                for (int i = 0; i < iterations; i++)
+                    using (await mutex.EnterAsync()) { }
+            }
+        }
+    }
+}
